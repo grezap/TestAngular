003a23ace89ccb7af8ce52e36e9180cf52fe5628
Grigorios Zapantis <gzapantis@FSHELLAS.LOCAL>
2018-06-15 19:01:02 +0300

    Include metronic template

--- AngularApp/src/app/app.module.ts
+++ AngularApp/src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { ScriptLoaderService } from "./_services/script-loader.service";
+import { ThemeComponent } from './theme/theme.component';
+import { ThemeRoutingModule } from "./theme/theme-routing.module";
+import { LayoutModule } from './theme/layouts/layout.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ThemeComponent
   ],
   imports: [
-    BrowserModule
+    LayoutModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    ThemeRoutingModule
   ],
-  providers: [],
+  providers: [ScriptLoaderService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
