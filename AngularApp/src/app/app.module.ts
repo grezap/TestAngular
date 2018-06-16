3a71eb91e6c97334d8010f5aca051ae490f09c62
unknown <gzapas@Wnd10Dev.gzapvm.local>
2018-06-16 21:28:34 +0300

    Necessary changes for minimal template

--- AngularApp/src/app/app.module.ts
+++ AngularApp/src/app/app.module.ts
@@ -1,24 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app-routing.module';
-import { ScriptLoaderService } from "./_services/script-loader.service";
 import { ThemeComponent } from './theme/theme.component';
-import { ThemeRoutingModule } from "./theme/theme-routing.module";
 import { LayoutModule } from './theme/layouts/layout.module';
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ScriptLoaderService } from "./_services/script-loader.service";
+import { ThemeRoutingModule } from "./theme/theme-routing.module";
+//import { AuthModule } from "./auth/auth.module";
 
 @NgModule({
   declarations: [
+    ThemeComponent,
     AppComponent,
-    ThemeComponent
   ],
   imports: [
     LayoutModule,
     BrowserModule,
     BrowserAnimationsModule,
     AppRoutingModule,
-    ThemeRoutingModule
+    ThemeRoutingModule,
+    //AuthModule,
   ],
   providers: [ScriptLoaderService],
   bootstrap: [AppComponent]
--- AngularApp/src/app/theme/theme-routing.module.ts
+++ AngularApp/src/app/theme/theme-routing.module.ts
@@ -1,34 +1,46 @@
-import {NgModule} from '@angular/core';
-import {ThemeComponent} from './theme.component';
-import {Routes, RouterModule} from '@angular/router';
-// import {AuthGuard} from '../auth/_guards/auth.guard';
+import { NgModule } from '@angular/core';
+import { ThemeComponent } from './theme.component';
+import { Routes, RouterModule } from '@angular/router';
+//import { AuthGuard } from "../auth/_guards/auth.guard";
 
 const routes: Routes = [
   {
-    'path': '',
-    'component': ThemeComponent,
-    // 'canActivate': [AuthGuard],
-    'children': [
-    //   {
-    //     'path': 'index',
-    //     'loadChildren': '.\/pages\/default\/blank\/blank.module#BlankModule',
-    //   },
-       {
-        'path': '',
-        'redirectTo': 'index',
-        'pathMatch': 'full',
+    "path": "",
+    "component": ThemeComponent,
+    //"canActivate": [AuthGuard],
+    "children": [
+      {
+        "path": "angular\/ng-bootstrap",
+        "loadChildren": ".\/pages\/default\/angular\/ng-bootstrap\/ng-bootstrap.module#NgBootstrapModule"
       },
-    ],
+      {
+        "path": "angular\/primeng",
+        "loadChildren": ".\/pages\/default\/angular\/primeng\/primeng.module#PrimengModule"
+      },
+      // {
+      //     "path": "index",
+      //     "loadChildren": ".\/pages\/default\/index\/index.module#IndexModule"
+      // },            
+      {
+        "path": "index",
+        "loadChildren": ".\/pages\/default\/blank\/blank.module#BlankModule"
+      },
+      {
+        "path": "",
+        "redirectTo": "index",
+        "pathMatch": "full"
+      }
+    ]
   },
   {
-    'path': '**',
-    'redirectTo': 'index',
-    'pathMatch': 'full',
-  },
+    "path": "**",
+    "redirectTo": "404",
+    "pathMatch": "full"
+  }
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule],
+  exports: [RouterModule]
 })
-export class ThemeRoutingModule {}
+export class ThemeRoutingModule { }
