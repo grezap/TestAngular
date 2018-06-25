da9b08392dba1b3518be2baa165e821d3d17bed7
Grigorios Zapantis <gzapantis@FSHELLAS.LOCAL>
2018-06-25 17:27:53 +0300

    Added more examples

---
+++ my-first-app/src/app/servers/servers.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-servers',
+  templateUrl: './servers.component.html',
+  styleUrls: ['./servers.component.css']
+})
+export class ServersComponent implements OnInit {
+
+  allowNewServers = false;
+  serverCreationStatus = 'No server was created';
+  serverName = 'Test Server';
+  @ViewChild('parag') parag: ElementRef;
+
+  constructor() {
+    setTimeout(() => { this.allowNewServers = true; }, 5000);
+  }
+
+  ngOnInit() {
+
+  }
+
+  // tslint:disable-next-line:use-life-cycle-interface
+  ngAfterViewInit() {
+    // Called after ngAfterContentInit when the component's view has been initialized. Applies to components only.
+    // Add 'implements AfterViewInit' to the class.
+    console.log(this.parag.nativeElement);
+  }
+
+  onCreateServer() {
+    this.serverCreationStatus = 'Server was created. Name is ' + this.serverName;
+  }
+
+  onUpdateServerName(event: Event) {
+    this.serverName = (<HTMLInputElement>event.target).value;
+  }
+
+}
