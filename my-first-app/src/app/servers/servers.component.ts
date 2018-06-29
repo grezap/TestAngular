747272ac1ccfda32f61fda97fa03c7708db96560
Grigorios Zapantis <gzapantis@FSHELLAS.LOCAL>
2018-06-29 12:24:46 +0300

    Minor Changes

--- my-first-app/src/app/servers/servers.component.ts
+++ my-first-app/src/app/servers/servers.component.ts
@@ -13,6 +13,7 @@ export class ServersComponent implements OnInit {
   serverName = '';
   // @ViewChild('parag') parag: ElementRef;
   serverCreated = false;
+  servers = ['Testserver', 'Testserver 2'];
 
   constructor() {
     setTimeout(() => { this.allowNewServers = true; }, 5000);
@@ -31,6 +32,7 @@ export class ServersComponent implements OnInit {
 
   onCreateServer() {
     this.serverCreated = true;
+    this.servers.push(this.serverName);
     this.serverCreationStatus = 'Server was created. Name is ' + this.serverName;
   }
 
